fe057788096ba55f085db7a111a9df9c21a6997a
ajpk <navyseals1405@gmail.com>
2024-09-09 17:44:34 -0400

    Liberty Scraper Progress
    Scrapes ~100 accordions, issues getting it to scrap every single one however

--- accordionscraper/spiders/accordionspider.py
+++ accordionscraper/spiders/accordionspider.py
@@ -13,21 +13,42 @@ class LibertySpider(scrapy.Spider):
 
     def parse(self, response):
         #price = response.css('p.cItemPrice::text').get()
+        pages = response.css('td.row.align-right > div > a')[2].get()
+        pages = int(pages[18])
 
+        shell_link = "https://www.libertybellows.com/shop/Piano-Accordions.htm?pageNum="
+        next_page = ""
+        for i in range(pages-1):
+            if i > 0:
+                next_page = shell_link + str(i+1)
+            else:
+                next_page = shell_link + "1"
+
+            yield response.follow(next_page, callback=self.site_crawl)
+
+    def site_crawl(self, response):
+        # get all prices on page into list
         for link in response.css('div.cItemDiv a::attr(href)'):
             yield response.follow(link.get(), callback=self.page_crawl)
 
     def page_crawl(self, response):
-       information = response.css('p#prod_description::text')
+       # For pages with video
+       information = response.css('p#prod_description::text')\
+
        data = []
        for info in information:
           data.append(info.get())
 
-       print(data)
+       # For pages with no video
+       if(len(data) == 0):
+            data = response.css('div.col-xs-12 > p')[1].get()
+            data = data.split("<br>")
 
        yield {
             'info': data
         }
+
+        # issue? 6799 - Blueberry Muffin Excelsior Accordiana Piano Accordion LM 41 120?
 
 class AlamoSpider(scrapy.Spider):
     name = 'alamo'
